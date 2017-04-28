c57a9482b6201cc27c82c2463849fe3ac8f06373
James Lannon <jamesmlannon1990@live.com>
2017-04-28 14:50:13 -0700

    change answer div to 4 new divs in html with varying messages and remove recommendedTrack span, hide these new divs in CSS, remove text insert function from initial branching in js that was linked to recommendedTrack span and changed to show and hide functions for new divs.

--- js/scripts.js
+++ js/scripts.js
@@ -7,22 +7,34 @@ $(function() {
     var track = $("input:radio[name=track]:checked").val();
 
     if ((typeOfWork === "designOne" && typeOfSite === "designThree") || (typeOfWork === "designOne" && track === "designFive") || (typeOfSite === "designThree" &&  track === "designFive")) {
-      $(".recommendedTrack").text("CSS/Design");
-      $(".answer").show();
+      $(".recommendDesign").show();
+      $(".recommendPhp").hide();
+      $(".recommendRuby").hide();
+      $(".recommendOther").hide();
     } else if ((typeOfWork === "rubyOne" && typeOfSite === "rubyThree") || (typeOfWork === "rubyOne" && track === "rubyFive") || (typeOfSite === "rubyThree" && track === "rubyFive")) {
-      $(".recommendedTrack").text("Ruby/Rails");
-      $(".answer").show();
+      $(".recommendRuby").show();
+      $(".recommendPhp").hide();
+      $(".recommendDesign").hide();
+      $(".recommendOther").hide();
     } else if ((typeOfWork === "phpOne" && typeOfSite === "phpThree") || (typeOfWork === "phpOne" && track === "phpFive") || (typeOfSite === "phpThree" && track === "phpFive")) {
-      $(".recommendedTrack").text("PHP/Drupal");
-      $(".answer").show();
+      $(".recommendPhp").show();
+      $(".recommendDesign").hide();
+      $(".recommendRuby").hide();
+      $(".recommendOther").hide();
     } else {
       $(".recommendedTrack").text("whatever you want it to be. You either have interests all across the board or just don't want to waste time with this quiz");
-      $(".answer").show();
+      $(".recommendOther").show();
+      $(".recommendPhp").hide();
+      $(".recommendRuby").hide();
+      $(".recommendDesign").hide();
     }
 
     if (name === "") {
       alert("Make sure you enter your name before submitting.")
-      $(".answer").hide();
+      $(".recommendOther").hide();
+      $(".recommendPhp").hide();
+      $(".recommendRuby").hide();
+      $(".recommendDesign").hide();
     }
 
     $(".name").text(name);
