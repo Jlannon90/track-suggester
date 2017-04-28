375e8a20b712e4c7989a426c7412937dc4688a1a
James Lannon <jamesmlannon1990@live.com>
2017-04-28 10:51:08 -0700

    complete branching for user input in radio boxes and result is inserted into recommendedTrack span

--- js/scripts.js
+++ js/scripts.js
@@ -7,18 +7,14 @@ $(function() {
 
     if ((typeOfWork === "designOne" && typeOfSite === "designThree") || (typeOfWork === "designOne" && track === "designFive") || (typeOfSite === "designThree" &&  track === "designFive")) {
       $(".recommendedTrack").text("CSS/Design");
-    } else if {
-
-
-      
+    } else if ((typeOfWork === "rubyOne" && typeOfSite === "rubyThree") || (typeOfWork === "rubyOne" && track === "rubyFive") || (typeOfSite === "rubyThree" && track === "rubyFive")) {
+      $(".recommendedTrack").text("Ruby/Rails");
+    } else if ((typeOfWork === "phpOne" && typeOfSite === "phpThree") || (typeOfWork === "phpOne" && track === "phpFive") || (typeOfSite === "phpThree" && track === "phpFive")) {
+      $(".recommendedTrack").text("PHP/Drupal");
+    } else {
+      $(".recommendedTrack").text("whatever you want it to be. You either have interests all across the board or just don't want to waste time with this quiz");
     }
-
-
-
-
-
-
+    
     $(".answer").show();
   });
-
 });
